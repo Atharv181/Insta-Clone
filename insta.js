0618785653e31e5e4932b505d931a4d2dbff2854
Atharv Bobade <atharvbobade181@gmail.com>
2021-08-20 19:26:52 +0530

    Initial commit

---
+++ insta.js
@@ -0,0 +1,16 @@
+
+let abc=document.getElementById("no1");
+abc.addEventListener("click", myFunction1);
+function myFunction1(){
+ window.location.href="https://www.instagram.com/_atharv_bobade_/";
+}
+let def=document.getElementById("no2");
+def.addEventListener("click", myFunction);
+function myFunction(){
+ window.location.href="https://www.instagram.com/vedanti_uttarwar/";
+}
+let ghi=document.getElementById("message");
+ghi.addEventListener("click", myFunction2);
+function myFunction2(){
+  window.location.href="message.html";
+}
---
+++ message.js
@@ -0,0 +1,5 @@
+let home=document.getElementById("homes");
+home.addEventListener("click",Function4);
+function Function4(){
+  window.location.href="insta.html";
+}
